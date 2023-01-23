a6a3bf29238ac3f31cb169994725ce80fb499688
Derek Brammeier <dbrammei@outlook.com>
2023-01-22 20:44:31 -0600

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_UlEcXPneT', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2qiiev7d8hlmf8k9oh59hjt9fq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
